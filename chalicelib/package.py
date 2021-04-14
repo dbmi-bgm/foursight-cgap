43263dcddef1efaf92206e04d53264d388c9dc6e
Eric Berg <Eric_Berg@hms.harvard.edu>
2021-04-14 08:44:27 -0400

    use foursight_core package main

--- chalicelib/package.py
+++ chalicelib/package.py
@@ -5,6 +5,7 @@ Takes on parameter for now: stage (either "dev" or "prod")
 from os.path import dirname
 import argparse
 from foursight_core.package import PackageDeploy as PackageDeploy_from_core
+from foursight_core.package import main as main_from_core
 
 
 class PackageDeploy(PackageDeploy_from_core):
@@ -16,22 +17,7 @@ class PackageDeploy(PackageDeploy_from_core):
 
 
 def main():
-    parser = argparse.ArgumentParser('chalice_package')
-    parser.add_argument(
-        'stage',
-        type=str,
-        choices=['dev', 'prod'],
-        help="chalice package stage. Must be one of 'prod' or 'dev'")
-    parser.add_argument(
-        'output_file',
-        type=str,
-        help='Directory where generated template should be written')
-    parser.add_argument(
-        '--merge_template',
-        type=str,
-        help='Location of a YAML template to be merged into the generated template')
-    args = parser.parse_args()
-    PackageDeploy.build_config_and_package(args)
+    main_from_core()
 
 
 if __name__ == '__main__':
