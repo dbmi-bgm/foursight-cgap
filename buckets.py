0f864f114ca180a193975964d96c0865f3d7e95a
SooLee <duplexa@gmail.com>
2021-06-14 13:56:06 -0400

    put urls here not in foursight-core

--- buckets.py
+++ buckets.py
@@ -9,6 +9,18 @@ class Buckets(Buckets_from_core):
     prefix = FOURSIGHT_PREFIX
     envs = ['cgap', 'cgapdev', 'cgaptest', 'cgapwolf']
 
+    def ff_url(self, env):
+        if env == 'cgap':
+            return 'https://cgap.hms.harvard.edu/'
+        else:
+            return 'http://%s.9wzadzju3p.us-east-1.elasticbeanstalk.com/' % self.ff_env(env)
+
+    def es_url(self, env):
+        if env == 'cgap':
+            return "https://search-%s-green-6-8-vj2hurnrw7cy4hnpgds7ttklnm.us-east-1.es.amazonaws.com" % env
+        else:
+            return "https://search-cgap-testing-6-8-vo4mdkmkshvmyddc65ux7dtaou.us-east-1.es.amazonaws.com"
+
 
 def main():
     buckets = Buckets()
