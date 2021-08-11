17a15e91a96e596911c5d0a5908359ce331f4640
Sarah <sarah_reiff@hms.harvard.edu>
2021-08-11 16:56:07 -0400

    edits to clone utils

--- chalicelib/checks/helpers/clone_utils.py
+++ chalicelib/checks/helpers/clone_utils.py
@@ -4,7 +4,8 @@ from magma_ff import import_metawfr
 # still need to figure if we need to add processed files to sample, sample_processing
 # hold off on meta-wfrs for now?
 
-pattern = re.compile(r'-v[0-9]+$')
+pattern_nospace = re.compile(r'-v[0-9]+$')
+pattern_pretty = re.compile(r' \(v[0-9]+\)$')
 
 
 class CaseToClone:
@@ -39,16 +40,19 @@ class CaseToClone:
         self.patch_individual_samples()
         self.new_sp_item = self.clone_sample_processing()
         self.new_cases = self.clone_cases()
-        self.analysis_type = self.get_analysis_type()
+        # self.analysis_type = self.get_analysis_type()
         # if self.metawf_uuid:
-        #     self.meta_wfr = self.add_metawfr()
+        #    self.meta_wfr = self.add_metawfr()
 
-    def append_version_to_value(self, value):
+    def append_version_to_value(self, value, pretty=False):
         if value is None:
             return
         if isinstance(value, list):
             return [self.append_version_to_value(item) for item in value if item]
+        pattern = pattern_nospace if not pretty else pattern_pretty
         new_value = re.sub(pattern, '', value)
+        if pretty:
+            return new_value + f' (v{self.new_version})'
         return new_value + '-v' + self.new_version
 
     def try_request(self, func, *args, **kwargs):
@@ -137,6 +141,7 @@ class CaseToClone:
             if item in self.sp_metadata:
                 new_sp_metadata[item] = self.sp_metadata.get(item)
         new_sp_metadata['samples'] = [item['new_id'] for item in self.sample_info.values()]
+        new_sp_metadata['analysis_version'] = 'v' + str(self.new_version)
 
         # add back some processed files, etc if pipeline is only being rerun at a particular step
         if self.sp_metadata.get('processed_files'):
@@ -170,6 +175,19 @@ class CaseToClone:
                 if field in old_case_metadata:
                     new_case_metadata[field] = old_case_metadata.get(field)
             new_case_metadata['sample_processing'] = self.new_sp_item
+            if not 'case_id' in new_case_metadata:
+                new_case_metadata['case_id'] = old_case_metadata.get('case_title')
+            new_case_metadata['case_id'] = self.append_version_to_value(new_case_metadata['case_id'], pretty=True)
+
+            if old_case_metadata.get('report'):
+                new_report_json = {
+                    'project': old_case_metadata['project'],
+                    'institution': old_case_metadata['institution']
+                }
+                report = try_request(ff_utils.post_metadata, new_report_json, 'report', key=self.key)
+                if report:
+                    new_case_metadata['report'] = report['@graph'][0]['@id']
+
             post_resp = try_request(ff_utils.post_metadata, new_case_metadata, 'case', key=self.key)
             if post_resp:
                 new_cases.append(post_resp['@graph'][0]['@id'])
@@ -182,23 +200,23 @@ class CaseToClone:
         elif sp_type.endswith('Group'):
             # figure out if trio+ or if parents aren't present
             sample_relations = [item.get('relationship') for item in self.sp_metadata.get('samples_pedigree', [{}])]
-            if all(relation in sample_relations for item in ['proband', 'mother', 'father']):
+            if all(item in sample_relations for item in ['proband', 'mother', 'father']):
                 return 'trio'
         # if not yet returned, then it is a proband-only analysis
         if all(sample.get('cram_files') for sample in self.samples_metadata):
             return 'cram proband'
         return 'proband'
 
-    # def add_metawfr(self):
-    #     metawfr_json = import_metawfr.import_metawfr(
-    #         metawf_uuid=self.metawf_uuid,
-    #         metawfr_uuid=self.case_metadata['meta_workflow_run'],
-    #         case_uuid=self.case_metadata['uuid'],
-    #         steps_name=self.steps_to_rerun,
-    #         create_metawfr=create_metawfr.create_metawfr_from_case,
-    #         type=f'WGS {self.analysis_type}',
-    #         ff_key=self.key,
-    #         post=False,
-    #         verbose=False
-    #     )
-    #     return metawfr_json
+    def add_metawfr(self):
+        metawfr_json = import_metawfr.import_metawfr(
+            metawf_uuid=self.metawf_uuid,
+            metawfr_uuid=self.case_metadata['meta_workflow_run'],
+            case_uuid=self.case_metadata['uuid'],
+            steps_name=self.steps_to_rerun,
+            create_metawfr=create_metawfr.create_metawfr_from_case,
+            type=f'WGS {self.analysis_type}',
+            ff_key=self.key,
+            post=False,
+            verbose=False
+        )
+        return metawfr_json
