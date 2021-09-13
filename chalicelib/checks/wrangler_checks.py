dd786873c0f9c2061b1f18ea896dfe0d5db75181
Sarah <sarah_reiff@hms.harvard.edu>
2021-09-13 13:53:39 -0400

    fix indents

--- chalicelib/checks/wrangler_checks.py
+++ chalicelib/checks/wrangler_checks.py
@@ -1251,7 +1251,7 @@ def get_metadata_for_cases_to_clone(connection, **kwargs):
         check.status = 'ERROR'
         return check
     meta_workflows = ff_utils.search_metadata(
-        f'search/?type=MetaWorkflow&version={version}&field=version&field=name&field=uuid'
+        f'search/?type=MetaWorkflow&version={version}&field=version&field=name&field=uuid',
         key=connection.ff_keys
     )
     if not meta_workflows:
@@ -1261,36 +1261,36 @@ def get_metadata_for_cases_to_clone(connection, **kwargs):
         check.status = 'ERROR'
         return check
     meta_workflow_dict = {mwf['name']: mwf for mwf in meta_workflows}
-output = {'run': {}, 'ignore': {}}
-for case in accessions:
-    case_metadata = ff_utils.get_metadata(case, key=keys['default'])
-    if case_metadata.get('superseded_by'):
-        output['ignore'][case] = 'This case has already been cloned.'
-        continue
-    mwfr = case_metadata.get('meta_workflow_run', {})
-    if not mwfr:
-        output['ignore'][case] = 'The case has no previous meta-workflow run. Skipping.'
-        continue
-    current_mwfr_version = mwfr.get('meta_workflow', {}).get('version')
-    if current_mwfr_version and current_mwfr_version.upper() == version.upper():
-        output['ignore'][case] = 'The case has already been run with this pipeline version.'
-        continue
-    updated_mwf = False
-    for k, v in meta_workflow_dict.items():
-        # TODO: this is a bit hacky right now, should change the mwf metadata to have title separate from version,
-        # and a calcprop that combines title and version
-        if k == mwfr.get('meta_workflow', {}).get('name'):
-            # value is a dict so that we can add more metadata in future iterations
-            output['run'][case] = {
-                'metawf_uuid': v['uuid'],
-                'create_SNV_mwfr': create_SNV_mwfr,
-                'keep_SV_mwfr': keep_SV_mwfr
-            }    
-            updated_mwf = True
-            break
-    if not updated_mwf:
-        output['ignore'][case] = f"{version} pipeline not found for this case's meta-workflow."
-        continue
+    output = {'run': {}, 'ignore': {}}
+    for case in accessions:
+        case_metadata = ff_utils.get_metadata(case, key=keys['default'])
+        if case_metadata.get('superseded_by'):
+            output['ignore'][case] = 'This case has already been cloned.'
+            continue
+        mwfr = case_metadata.get('meta_workflow_run', {})
+        if not mwfr:
+            output['ignore'][case] = 'The case has no previous meta-workflow run. Skipping.'
+            continue
+        current_mwfr_version = mwfr.get('meta_workflow', {}).get('version')
+        if current_mwfr_version and current_mwfr_version.upper() == version.upper():
+            output['ignore'][case] = 'The case has already been run with this pipeline version.'
+            continue
+        updated_mwf = False
+        for k, v in meta_workflow_dict.items():
+            # TODO: this is a bit hacky right now, should change the mwf metadata to have title separate from version,
+            # and a calcprop that combines title and version
+            if k == mwfr.get('meta_workflow', {}).get('name'):
+                # value is a dict so that we can add more metadata in future iterations
+                output['run'][case] = {
+                    'metawf_uuid': v['uuid'],
+                    'create_SNV_mwfr': create_SNV_mwfr,
+                    'keep_SV_mwfr': keep_SV_mwfr
+                }
+                updated_mwf = True
+                break
+        if not updated_mwf:
+            output['ignore'][case] = f"{version} pipeline not found for this case's meta-workflow."
+            continue
 
     check.full_output = output
     check.status = 'PASS'
