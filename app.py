66d426e04ce183bdec69e5eaa1ecf6295d8b0dc0
William Ronchetti <wrr33@cornell.edu>
2021-10-18 12:01:29 -0400

    new beta, clarify app.py w comment

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+# VERY IMPORTANT:
+# In order to support extension via 4dn-cloud-infra,
+# this app.py is effectively an alias for chalicelib/app.py.
+# In theory, any repository could include foursight-cgap as a
+# dependency and selectively import functionality from
+# chalicelib.app. In the case of 4dn-cloud-infra, we import nothing
+# from this file and rewrite the core application code. In the case
+# of this repository though, this app.py is the "real" one (and
+# notably is not a part of the library). This gives us the best of
+# both worlds in that we can use foursight-cgap as an importable
+# library while maintaining application (functional) isolation
+# and respective differences in environments, check scheduling and
+# the check code itself. -- Will Oct 18 2021
 from chalicelib.app import *
