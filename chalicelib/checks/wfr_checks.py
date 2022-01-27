0478ff78c508d1588a50d190685ca3be1bff8af3
William Ronchetti <wrr33@cornell.edu>
2022-01-27 13:12:17 -0500

    thug commit: lowercase v

--- chalicelib/checks/wfr_checks.py
+++ chalicelib/checks/wfr_checks.py
@@ -19,7 +19,7 @@ default_pipelines_to_run = ['CNV v2', 'WGS Trio v26',
     'WGS Proband-only Cram v26', 'WES Proband-only v26', 'WES Family v26', 'WES Trio v26',
     'WGS Proband-only v26', 'WGS Family v26', 'WGS Trio v27', 'WGS Proband-only Cram v27',
     'WES Proband-only v27', 'WES Family v27', 'WES Trio v27', 'WGS Proband-only v27',
-    'WGS Family v27', 'SV Germline V3']
+    'WGS Family v27', 'SV Germline v3']
 
 
 @check_function(file_type='File', start_date=None)
