d1026b786059ac89c660aa4272366580c21b7971
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2022-05-13 10:23:52 -0400

    Revision

--- chalicelib/checks/helpers/lifecycle_utils.py
+++ chalicelib/checks/helpers/lifecycle_utils.py
@@ -5,33 +5,50 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=2)
 
-lifecycle_status = {
-    "standard": "Standard",
-    "ia": "Infrequent Access",
-    "da": "Deep Archive",
-    "deleted": "Deleted",
-}
+# Schema constants
+RESULT = "result"
+PERSISTENT_RESULT = "persistent_result"
+READS = "reads"
+INTERMEDIATE_OUTPUT = "intermediate_output"
+INTERMEDIATE_RESULT = "intermediate_result"
+MOVE_TO_INFREQUENT_ACCESS_AFTER = "move_to_infrequent_access_after"
+MOVE_TO_DEEP_ARCHIVE_AFTER = "move_to_deep_archive_after"
+EXPIRE_AFTER = "expire_after"
+STANDARD = "standard"
+INFREQUENT_ACCESS = "infrequent access"
+DEEP_ARCHIVE = "deep archive"
+DELETED = "deleted"
+IGNORE = "ignore"
+PENDING = "pending"
+COMPLETE = "complete"
+
+# lifecycle_status = {
+#     "standard": "Standard",
+#     "ia": "Infrequent Access",
+#     "da": "Deep Archive",
+#     "deleted": "Deleted",
+# }
 
 default_lifecycle_policy = {
-    "result": {
-        "move_to_infrequent_access_after": 0,
-        "move_to_deep_archive_after": 3,
-        "expire_after": 36,
+    RESULT: {
+        MOVE_TO_INFREQUENT_ACCESS_AFTER: 0,
+        MOVE_TO_DEEP_ARCHIVE_AFTER: 3,
+        EXPIRE_AFTER: 36,
     },
-    "persistent_result": {
-        "move_to_infrequent_access_after": 0,
-        "expire_after": 36,
+    PERSISTENT_RESULT: {
+        MOVE_TO_INFREQUENT_ACCESS_AFTER: 0,
+        EXPIRE_AFTER: 36,
     },
-    "reads": {
-        "move_to_deep_archive_after": 0,
-        "expire_after": 12,
+    READS: {
+        MOVE_TO_DEEP_ARCHIVE_AFTER: 0,
+        EXPIRE_AFTER: 12,
     },
-    "intermediate_output": {
-        "expire_after": 0,
+    INTERMEDIATE_OUTPUT: {
+        EXPIRE_AFTER: 0,
     },
-    "intermediate_result": {
-        "move_to_deep_archive_after": 0,
-        "expire_after": 36,
+    INTERMEDIATE_RESULT: {
+        MOVE_TO_DEEP_ARCHIVE_AFTER: 0,
+        EXPIRE_AFTER: 36,
     },
 }
 
@@ -39,20 +56,45 @@ default_lifecycle_policy = {
 
 
 
+def should_mwfr_be_checked(metawfr, max_checking_frequency):
+    """This function determines if a MetaWorkflowRun should be checked for lifecycle updates
 
+    Args:
+        metawfr(dict): MetaWorkflowRun from portal
+        max_checking_frequency (int): determines how often a metawfr is checked at most (in days). Default 7 (days).
 
+    Returns:
+        boolean
+    """
+    # TODO: Add stopped, inactive here as well?
+    valid_final_status = ["completed"]  # Only these are checked in the following
+    if metawfr["final_status"] not in valid_final_status:
+        return False
 
+    # Check when the MWFR has been created. If it is younger than 2 weeks, don't check it
+    now = datetime.utcnow()
+    date_created = convert_es_timestamp_to_datetime(metawfr["date_created"])
+    metawfr_age = now - date_created
+    if metawfr_age.total_seconds() < 14 * 24 * 60 * 60:
+        return False
 
+    # If lifecycle_status is present, the metawfr was been checked before.
+    # Make sure enough time passed to check it again
+    # TODO Check the following, once the metatdata is there
+    if "lifecycle_status" in metawfr:
+        metawfr_lifecycle_info = metawfr["lifecycle_status"]
+        last_checked = convert_es_timestamp_to_datetime(metawfr_lifecycle_info["last_checked"])
+        status = metawfr_lifecycle_info["status"]
+        delta = now - last_checked
+        # check this metawfr at most every {max_checking_frequency} days
+        if delta.total_seconds() < max_checking_frequency * 24 * 60 * 60:
+            return False
 
+        if status == "pending":
+            return True
 
-# TODO To implement
-def has_bam_qc_passed():
-    return True
+    return False
 
-
-# TODO To implement
-def are_variants_ingested():
-    return True
 
 
 def get_lifecycle_category(file_metadata):
@@ -68,13 +110,14 @@ def get_lifecycle_category(file_metadata):
     file_format = file_metadata.get("file_format").get("file_format")
     file_type = file_metadata.get("file_type")
     if file_format in ["fastq", "cram"]:
-        return "reads"
+        return READS
     elif file_format in ["bam"] and file_type == "alignments":
-        return "result"
+        return RESULT
     elif file_format in ["bam"] and file_type == "intermediate file":
-        return "intermediate_output"
+        return INTERMEDIATE_OUTPUT
     else:
         return None
+
 
 def get_workflow_lifecycle_category_map(meta_workflow):
     """This function extracts a mapping "workflow name -> lifecycle category" from a meta workflow
@@ -101,33 +144,34 @@ def get_workflow_lifecycle_category_map(meta_workflow):
 
     return mapping
 
-        
 
+def get_file_lifecycle_status(file_metadata, file_lifecycle_policy):
+    """This function returns the correct lifecycle status for a given file, i.e.
+       which S3 storage class it should currently be in.
 
+    Args:
+        file_metadata(dict) : file meta data from portal
+        file_lifecycle_policy (dict) : Policy for that file, e.g. {MOVE_TO_DEEP_ARCHIVE_AFTER: 0, EXPIRE_AFTER: 12}
 
-# get the correct lifecycle status for a file
-def get_lifecycle_status(file_metadata, file_lifecycle_policy):
+    Returns:
+        string : correct lifecycle status of file given its lifecycle policy and age
+    """
 
     date_created = convert_es_timestamp_to_datetime(file_metadata.get("date_created"))
-    date_new = datetime.utcnow()
+    now = datetime.utcnow()
     # We are using the file creation for simplicity, we should use the time from when the workflow run
     # completed successfully. We asssume that those dates are sufficiently close.
-    file_age = (date_new - date_created).days / 30  # in months
-    #print(file_age, "months", date_created, date_new)
-    #pp.pprint(file_lifecycle_policy)
+    file_age = (now - date_created).days / 30  # in months
 
     # Find the lifecycle policy category that is currently applicable
     active_categories = {k: v for (k, v) in file_lifecycle_policy.items() if v < file_age}
     current_category = max(active_categories, key=active_categories.get)
 
-    # print(active_categories, current_category)
-    # print(map_lifecycle_policy_to_status(current_category))
-    # pp.pprint(file_metadata)
-    return map_lifecycle_policy_to_status(current_category)
+    return lifecycle_policy_to_status(current_category)
 
 
-def map_lifecycle_policy_to_status(policy_category):
-    """Converts a lifecycle policy category (e.g. "move_to_deep_archive_after") to the corresponding status (e.g. "da").
+def lifecycle_policy_to_status(policy_category):
+    """Converts a lifecycle policy category (e.g. "move_to_deep_archive_after") to the corresponding status (e.g. "deep archive").
 
     Args:
         policy_category(string): lifecycle policy category (e.g. "move_to_deep_archive_after")
@@ -135,14 +179,33 @@ def map_lifecycle_policy_to_status(policy_category):
     Returns:
         A string : lifecylce status (defaults to "standard")
     """
-    if policy_category == "move_to_infrequent_access_after":
-        return "ia"
-    elif policy_category == "move_to_deep_archive_after":
-        return "da"
-    elif policy_category == "expire_after":
-        return "deleted"
+    if policy_category == MOVE_TO_INFREQUENT_ACCESS_AFTER:
+        return INFREQUENT_ACCESS
+    elif policy_category == MOVE_TO_DEEP_ARCHIVE_AFTER:
+        return DEEP_ARCHIVE
+    elif policy_category == EXPIRE_AFTER:
+        return DELETED
     else:
-        return "standard"
+        return STANDARD
+
+
+def lifecycle_status_to_s3_tag(lifecycle_status):
+    """Converts a lifecycle status to the corresponding S3 tag.
+
+    Args:
+        lifecycle_status(string): lifecycle status from portal (e.g. "deep archive")
+
+    Returns:
+        A list of tags (dicts) : S3 tags (defaults to empty list)
+    """
+    if lifecycle_status == INFREQUENT_ACCESS:
+        return [{'Key': 'Lifecycle','Value': 'IA'}]
+    elif lifecycle_status == DEEP_ARCHIVE:
+        return [{'Key': 'Lifecycle','Value': 'GlacierDA'}]
+    elif lifecycle_status == DELETED:
+        return [{'Key': 'Lifecycle','Value': 'expire'}]
+    else:
+        return []
 
 
 def convert_es_timestamp_to_datetime(raw):
--- chalicelib/checks/lifecycle_checks.py
+++ chalicelib/checks/lifecycle_checks.py
@@ -1,7 +1,7 @@
 import json
 import random
 import pprint
-from datetime import datetime
+import datetime
 from dcicutils import ff_utils, s3Utils
 
 from .helpers import lifecycle_utils
@@ -30,7 +30,6 @@ def check_metawfrs_lifecycle_status(connection, **kwargs):
     check.description = (
         "Inspect metaworkflow runs and find files whose lifecycle status needs patching"
     )
-    check.brief_output = {}
     check.summary = ""
     check.full_output = {}
     check.status = "PASS"
@@ -43,12 +42,13 @@ def check_metawfrs_lifecycle_status(connection, **kwargs):
     num_metawfrs_in_portal = int(counts_info_metawfrs.split()[1])
     num_metawfrs_to_check = kwargs.get("metawfrs_per_run", 5)
     max_checking_frequency = kwargs.get("max_checking_frequency", 7)
-    print(num_metawfrs_in_portal, num_metawfrs_to_check)
+    print("num_metawfrs_in_portal", num_metawfrs_in_portal)
+    print("num_metawfrs_to_check", num_metawfrs_to_check)
 
     # Get {num_metawfrs_to_check} random MetaWorkflowRuns
     limit = str(num_metawfrs_to_check)
     search_from = str(random.randint(0, num_metawfrs_in_portal))
-    search_from = str(78)
+    search_from = str(10)
     search_metawfrs = (
         "/search/?type=MetaWorkflowRun" + "&limit=" + limit + "&from=" + search_from
     )
@@ -56,43 +56,20 @@ def check_metawfrs_lifecycle_status(connection, **kwargs):
     print(search_metawfrs)
 
     # This will contain the metawfr that have been processed and require meta data updates
-    metawfr_to_update = []
+    metawfrs_to_update = []
     files_without_lifecycle_category = []
     files_to_update = []
-    files_to_update_brief = [] # This will be used for display in check/action output
 
     for metawfr in result_metawfrs:
 
-        # TODO: Add stopped here as well?
-        valid_final_status = ["completed"]  # Only these are checked in the following
-        if metawfr["final_status"] not in valid_final_status:
+        if not lifecycle_utils.should_mwfr_be_checked(metawfr, max_checking_frequency):
             continue
-
-        # If lifecycle_info is present, the metawfr was been checked before.
-        # Make sure enough time passed to check it again
-        # TODO Check the following, once the metatdat is there
-        if "lifecycle_info" in metawfr:
-            metawfr_lifecycle_info = metawfr["lifecycle_info"]
-            now = datetime.datetime.utcnow()
-            last_checked = metawfr_lifecycle_info["last_checked"]
-            delta = now - last_checked
-            # check this metawfr at most every {max_checking_frequency} days
-            if delta.total_seconds() < max_checking_frequency * 24 * 60 * 60:
-                continue
-
-        if not lifecycle_utils.has_bam_qc_passed():
-            continue
-
-        if not lifecycle_utils.are_variants_ingested():
-            continue
-
-        metawfr_to_update.append(metawfr)
 
         lifecycle_policy = lifecycle_utils.default_lifecycle_policy
         if "lifecycle_policy" in metawfr["project"]:
             lifecycle_policy = metawfr["project"]["lifecycle_policy"]
 
-        # Information about the lifecycle categories of the files
+        # Information about the lifecycle categories of workflow files
         # can be found in the meta workflow (custom_pf_field of each workflow)
         meta_workflow_uuid = metawfr["meta_workflow"]["uuid"]
         meta_workflow = ff_utils.get_metadata(meta_workflow_uuid, key=my_auth)
@@ -125,72 +102,79 @@ def check_metawfrs_lifecycle_status(connection, **kwargs):
                     "wf_name": wf_name
                 })
 
+        # The lifecycle status of the metawfr will be "pending" if there exists an associated file
+        # that is not deleted or ignored
+        metawfr_lifecycle_status = lifecycle_utils.COMPLETE
+
         for file in all_files:
             file_metadata = ff_utils.get_metadata(file["uuid"], key=my_auth)
 
             # This is a best guess of the lifecycle category.
             # Input files don't have their lifecycle category stored in the metadata,
             # therefore we have to rely on this function for those
-            lifecycle_category = lifecycle_utils.get_lifecycle_category(file_metadata)
+            file_lifecycle_category = lifecycle_utils.get_lifecycle_category(file_metadata)
 
             # If we deal with a workflow output file, try to replace the lifecycle category with the one
             # attached to the corresponding metadata.
             wf_name = file.get("wf_name")
-            if wf_name and (wf_name in wf_lc_map):
-                lifecycle_category = wf_lc_map[wf_name]
+            if wf_name:
+                file_lifecycle_category = wf_lc_map.get(wf_name)
 
-            if not lifecycle_category:
+            if not file_lifecycle_category:
                 check.status = "WARN"
-                check.summary = (
-                    "Could not assign a lifecycle category to some files."
-                )
-                check.description = check.summary
+                check.warning = "Could not assign a lifecycle category to some files."
                 files_without_lifecycle_category.append(file)
+                metawfr_lifecycle_status = lifecycle_utils.PENDING
             else:
-                old_lifecycle_status = file_metadata.get("lifecycle_status")
-                new_lifecycle_status = lifecycle_utils.get_lifecycle_status(
-                    file_metadata, lifecycle_policy.get(lifecycle_category)
+                old_file_lifecycle_status = file_metadata.get("s3_lifecycle_status")
+                if old_file_lifecycle_status == lifecycle_utils.IGNORE:
+                    continue
+                
+                file_lifecycle_policy = lifecycle_policy.get(file_lifecycle_category)
+                new_file_lifecycle_status = lifecycle_utils.get_file_lifecycle_status(
+                    file_metadata, file_lifecycle_policy
                 )
 
-                if old_lifecycle_status != new_lifecycle_status:
-                    files_to_update.append(
-                        {
-                            "file_metadata": file_metadata,
-                            "new_lifecycle_status": new_lifecycle_status,
-                            "metawfr_uuid": metawfr[
-                                "uuid"
-                            ], 
-                        }
-                    )
-                    files_to_update_brief.append(
-                        {
+                if new_file_lifecycle_status != lifecycle_utils.DELETED:
+                    metawfr_lifecycle_status = lifecycle_utils.PENDING
+
+                if old_file_lifecycle_status != new_file_lifecycle_status:
+                    update_dict = {
                             "uuid": file_metadata["uuid"],
                             "upload_key": file_metadata["upload_key"],
-                            "old_lifecycle_status": old_lifecycle_status,
-                            "new_lifecycle_status": new_lifecycle_status,
-                            "metawfr_uuid": metawfr[
-                                "uuid"
-                            ], 
+                            "old_lifecycle_status": old_file_lifecycle_status,
+                            "new_lifecycle_status": new_file_lifecycle_status,
+                            "metawfr_uuid": metawfr["uuid"], 
+                            "is_extra_file": False
                         }
-                    )
-           
-        #     pp.pprint(workflow)
+                    files_to_update.append(update_dict)
 
-        print(metawfr["uuid"], metawfr["final_status"])
-        # pp.pprint(metawfr)
-    if len(files_to_update) == 0:
-        check.summary = "There are no files that need patching."
+                    # Get extra files and update those as well. They will be treated like the original file
+                    extra_files = file_metadata.get("extra_files")
+                    for ef in extra_files:
+                        ef_update_dict = update_dict.copy()
+                        ef_update_dict["upload_key"] = ef["upload_key"]
+                        ef_update_dict["is_extra_file"] = True
+                        files_to_update.append(ef_update_dict)
+                        
+
+        # Update the lifecycle status of the metawfr itself. It has status "pending" until 
+        # all files associated with it have been deleted or can be ignored.
+        now_es = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        metawfrs_to_update.append(
+            {
+                "uuid": metawfr["uuid"],
+                "new_lifecycle_status": metawfr_lifecycle_status,
+                "last_checked": now_es
+            }
+        )
+
+    check.summary = f'{len(files_to_update)} files in {len(metawfrs_to_update)} MetaWorkflowRuns require patching.'
 
     check.full_output = {
-        "metawfr_to_update": metawfr_to_update,
+        "metawfrs_to_update": metawfrs_to_update,
         "files_without_lifecycle_category": files_without_lifecycle_category,
         "files_to_update": files_to_update,
-    }
-
-    check.brief_output = {
-        "metawfr_to_update": [d["uuid"] for d in metawfr_to_update],
-        "files_without_lifecycle_category": files_without_lifecycle_category,
-        "files_to_update": files_to_update_brief,
     }
 
     return check
@@ -201,69 +185,71 @@ def check_metawfrs_lifecycle_status(connection, **kwargs):
 def patch_file_lifecycle_status(connection, **kwargs):
     # start = datetime.utcnow()
     action = ActionResult(connection, 'patch_file_lifecycle_status')
-    action_logs = {'metawfrs_updated': [], 'metawfrs_that_failed_linecount_test': []}
     my_auth = connection.ff_keys
     env = connection.ff_env
+    my_s3_util = s3Utils(env=env)
+    raw_bucket = my_s3_util.raw_file_bucket
+    out_bucket = my_s3_util.outfile_bucket
     check_result = action.get_associated_check_result(kwargs)
     check_output = check_result.get('full_output', {})
-    check_brief_output = check_result.get('brief_output', {})
-    action_logs['check_output'] = check_brief_output
+    action_logs = {}
+    action_logs['check_output'] = check_output
     action_logs['patched_metawfrs'] = []
     action_logs['patched_files'] = []
     action_logs['error'] = []
 
     files = check_output.get('files_to_update', [])
-    metawfrs = check_output.get('metawfr_to_update', [])
-    for metawfr in metawfrs:
-        metawfr_uuid = metawfr["uuid"]
-        metawfr_files = [f for f in files if f['metawfr_uuid'] == metawfr_uuid]
-        action_logs['patched_metawfrs'].append(metawfr_uuid)
-        for metawfr_file in metawfr_files:
-            file_uuid = metawfr_file["file_metadata"]["uuid"]
-            action_logs['patched_files'].append(file_uuid)
+    metawfrs = check_output.get('metawfrs_to_update', [])
 
-    # for metawfr_uuid in metawfr_uuids:
-    #     now = datetime.utcnow()
-    #     if (now-start).seconds > lambda_limit:
-    #         action.description = 'Did not complete action due to time limitations'
-    #         break
-    #     try:
-    #         metawfr_meta = ff_utils.get_metadata(metawfr_uuid, add_on='?frame=raw', key=my_auth)
-    #         # we have a few different dictionaries of steps to check output from in linecount_dicts.py
-    #         # the proband-only and family workflows have the same steps, so we assign the proband_SNV_dict
-    #         if 'Proband-only' in metawfr_meta['title'] or 'Family' in metawfr_meta['title']:
-    #             steps_dict = proband_SNV_dict
-    #         # trio has novoCaller, so it has a separate dictionary of steps
-    #         elif 'Trio' in metawfr_meta['title']:
-    #             steps_dict = trio_SNV_dict
-    #         # cnv/sv is a completely different pipeline, so has a many different steps
-    #         elif 'CNV' in metawfr_meta['title']:
-    #             steps_dict = CNV_dict
-    #         # if this is run on something other than those expected MWFRs, we want an error.
-    #         else:
-    #             e = 'Unexpected MWF Title: '+metawfr_meta['title']
-    #             action_logs['error'].append(str(e))
-    #             continue
-    #         # this calls check_lines from cgap-pipeline pipeline_utils check_lines.py (might get moved to generic repo in the future)
-    #         # will return TRUE or FALSE if all pipeline steps are fine, or if there are any that do not match linecount with their partners, respectively
-    #         linecount_result = check_lines(metawfr_uuid, my_auth, steps=steps_dict, fastqs=fastqs_dict)
-    #         #want an empty dictionary if no overall_qcs, or a dictionary of tests and results if there are items in the overall_qcs list
-    #         overall_qcs_dict = {qc['name']: qc['value'] for qc in metawfr_meta.get('overall_qcs', [])}
-    #         overall_qcs_dict['linecount_test'] = 'PASS' if linecount_result else 'FAIL'
-    #         # turn the dictionary back into a list of dictionaries that is properly structured (e.g., overall_qcs: [{"name": "linecount_test", "value": "PASS"}, {...}, {...}])
-    #         updated_overall_qcs = [{'name': k, 'value': v} for k, v in overall_qcs_dict.items()]
-    #         try:
-    #             ff_utils.patch_metadata({'overall_qcs': updated_overall_qcs}, metawfr_uuid, key=my_auth)
-    #             if linecount_result:
-    #                 action_logs['metawfrs_that_passed_linecount_test'].append(metawfr_uuid)
-    #             else:
-    #                 action_logs['metawfrs_that_failed_linecount_test'].append(metawfr_uuid)
-    #         except Exception as e:
-    #             action_logs['error'].append(str(e))
-    #             continue
-    #     except Exception as e:
-    #         action_logs['error'].append(str(e))
-    #         continue
+    for metawfr_dict in metawfrs:
+        metawfr_uuid = metawfr_dict["uuid"]
+        metawfr_files = [f for f in files if f['metawfr_uuid'] == metawfr_uuid]
+        
+        for metawfr_file in metawfr_files:
+            f_uuid = metawfr_file["uuid"]
+            f_upload_key = metawfr_file["upload_key"]
+            f_ols = metawfr_file["old_lifecycle_status"]
+            f_nls = metawfr_file["new_lifecycle_status"]
+            f_is_extra = metawfr_file["is_extra_file"]
+
+            # Before tagging the file, we need to verify that it actually exists on S3. However, the correct
+            # bucket cannot be easily inferred from the file meta data currently. Most files will be
+            # in the out_bucket.
+            f_bucket = None 
+            if my_s3_util.does_key_exist(f_upload_key, bucket=out_bucket, print_error=False):
+                f_bucket = out_bucket
+            elif my_s3_util.does_key_exist(f_upload_key, bucket=raw_bucket, print_error=False):
+                f_bucket = raw_bucket
+            if not f_bucket:
+                action_logs['error'].append(f'Cannot patch file {f_uuid}: not found on S3')
+                continue
+
+            try:
+                if not f_is_extra:
+                    ff_utils.patch_metadata({'s3_lifecycle_status': f_nls}, f_uuid, key=my_auth)
+                s3_tag = lifecycle_utils.lifecycle_status_to_s3_tag(f_nls)
+                if s3_tag:
+                    my_s3_util.set_object_tags(f_upload_key, f_bucket, s3_tag, replace_tags=True)
+                else: 
+                    raise Exception("Could not determine S3 tag")
+                action_logs['patched_files'].append(f'Lifecycle status of file {f_uuid} changed from {f_ols} to {f_nls}')
+            
+            except Exception as e:
+                action_logs['error'].append(f'Error patching or tagging file {f_uuid}: {str(e)}')
+                continue
+
+        try:
+            updated_mwfr_lifecycle_status = {
+                "status": metawfr_dict["new_lifecycle_status"],
+                "last_checked": metawfr_dict["last_checked"],
+            }
+            ff_utils.patch_metadata({'lifecycle_status': updated_mwfr_lifecycle_status}, metawfr_uuid, key=my_auth)
+            action_logs['patched_metawfrs'].append(metawfr_uuid)
+
+        except Exception as e:
+            action_logs['error'].append(f'Error patching MetaWorkflowRun {metawfr_uuid}: {str(e)}')
+            continue
+
     action.output = action_logs
     # we want to display an error if there are any errors in the run, even if many patches are successful
     if action_logs['error'] == []:
