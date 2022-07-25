1165a06291a03bda44166e978d6b3babd76c1a05
David Michaels <dmichaels@gmail.com>
2022-07-25 13:54:42 -0400

    First cut at IDENTITY-izing Foursight.

--- chalicelib/app_utils.py
+++ chalicelib/app_utils.py
@@ -1,3 +1,4 @@
+import os
 from os.path import dirname
 from foursight_core.app_utils import AppUtils as AppUtils_from_core
 from .vars import FOURSIGHT_PREFIX, HOST
@@ -12,3 +13,14 @@ class AppUtils(AppUtils_from_core):
     package_name = 'chalicelib'
     check_setup_dir = dirname(__file__)
     html_main_title = 'Foursight-CGAP'
+
+    # dmichaels/2022-07-20:
+    # Added this to get the ES_HOST value from via IDENTITY (from GAC).
+    # which was applied globally (to os.environ) in foursight-core,
+    # via AppUtils_from_core; to override the default self.host
+    # value set above to the correct value (C4-826).
+    def __init__(self):
+        super().__init__()
+        es_host_from_foursight_core_apply_identity = os.environ.get("ES_HOST")
+        if es_host_from_foursight_core_apply_identity:
+            self.host = es_host_from_foursight_core_apply_identity
