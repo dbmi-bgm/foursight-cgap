9f3489733b5a7b7cfd70d4429f436893fd5e2136
David Michaels <dmichaels@gmail.com>
2022-07-28 08:36:17 -0400

    Backed out addition of AppUtils constructor as foursight-core apply_identity is done more statically now.

--- chalicelib/app_utils.py
+++ chalicelib/app_utils.py
@@ -6,21 +6,15 @@ from .vars import FOURSIGHT_PREFIX, HOST
 
 class AppUtils(AppUtils_from_core):
 
+    es_host_from_foursight_core_apply_identity = os.environ.get("ES_HOST")
+    if es_host_from_foursight_core_apply_identity:
+        HOST = es_host_from_foursight_core_apply_identity
+
     # overwriting parent class
     prefix = FOURSIGHT_PREFIX
     FAVICON = 'https://cgap-dbmi.hms.harvard.edu/favicon.ico'
+    es_host_from_foursight_core_apply_identity
     host = HOST
     package_name = 'chalicelib'
     check_setup_dir = dirname(__file__)
     html_main_title = 'Foursight-CGAP'
-
-    # dmichaels/2022-07-20/C4-826:
-    # Added this to get the ES_HOST value from via IDENTITY (from GAC).
-    # which was applied globally (to os.environ) in foursight-core,
-    # via AppUtils_from_core; to override the default self.host
-    # value set above to the correct value.
-    def __init__(self):
-        super().__init__()
-        es_host_from_foursight_core_apply_identity = os.environ.get("ES_HOST")
-        if es_host_from_foursight_core_apply_identity:
-            self.host = es_host_from_foursight_core_apply_identity
