70242a8164d72160a18b63d46ef5f1222088090c
David Michaels <david_michaels@hms.harvard.edu>
2022-11-28 11:23:16 -0500

    Minor code cleanup.

--- chalicelib_cgap/app_utils.py
+++ chalicelib_cgap/app_utils.py
@@ -24,7 +24,7 @@ class AppUtils(AppUtils_from_core):
     prefix = FOURSIGHT_PREFIX
     FAVICON = 'https://cgap-dbmi.hms.harvard.edu/favicon.ico'
     host = HOST
-    AppUtils_from_core.package_name = 'chalicelib_cgap'
+    package_name = 'chalicelib_cgap'
 
     DEFAULT_ENV = os.environ.get("ENV_NAME", "foursight-cgap-env-uninitialized")
 
